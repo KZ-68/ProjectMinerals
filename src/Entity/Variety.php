68990398c9222ee5a35493827db456b34b795422
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-01 23:00:57 +0200

    feat: add images collection attributes

--- src/Entity/Variety.php
+++ src/Entity/Variety.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\VarietyRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: VarietyRepository::class)]
@@ -18,6 +20,14 @@ class Variety
 
     #[ORM\ManyToOne(inversedBy: 'varieties')]
     private ?Mineral $mineral = null;
+
+    #[ORM\OneToMany(mappedBy: 'variety', targetEntity: Image::class)]
+    private Collection $images;
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +54,36 @@ class Variety
     public function setMineral(?Mineral $mineral): static
     {
         $this->mineral = $mineral;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Image>
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): static
+    {
+        if (!$this->images->contains($image)) {
+            $this->images->add($image);
+            $image->setVariety($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): static
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getVariety() === $this) {
+                $image->setVariety(null);
+            }
+        }
 
         return $this;
     }
