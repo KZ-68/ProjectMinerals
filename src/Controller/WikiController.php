6df7960d43a08bad32622d671e8da73e40d2b042
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-01 23:08:30 +0200

    feat: add select method for Mineral Page

--- src/Controller/WikiController.php
+++ src/Controller/WikiController.php
@@ -6,6 +6,7 @@ use App\Entity\Mineral;
 use App\Entity\Category;
 use App\Repository\MineralRepository;
 use App\Repository\CategoryRepository;
+use App\Repository\ImageRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,9 +23,11 @@ class WikiController extends AbstractController
     }
 
     #[Route('/wiki/mineral/{id}/show', name: 'show_mineral')]
-    public function showMineral(Mineral $mineral): Response
+    public function showMineral(Mineral $mineral, ImageRepository $imageRepository ): Response
     {
-        return $this->render('mineral/showMineral.html.twig', [
+        $images = $imageRepository->findImagesAndNameInMineral($mineral->getId());
+        return $this->render('wiki/showMineral.html.twig', [
+            'images' => $images,
             'mineral' => $mineral
         ]);
     }
