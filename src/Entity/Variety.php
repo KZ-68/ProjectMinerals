c680929e34c716ff1f612c800adcd34a1f1d0f14
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-01 16:31:36 +0200

    feat: add Variety Entity

---
+++ src/Entity/Variety.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VarietyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VarietyRepository::class)]
+class Variety
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'varieties')]
+    private ?Mineral $mineral = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMineral(): ?Mineral
+    {
+        return $this->mineral;
+    }
+
+    public function setMineral(?Mineral $mineral): static
+    {
+        $this->mineral = $mineral;
+
+        return $this;
+    }
+}
