4ee3c143f8841eda4580d35f2c7471dea46536d9
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-01 16:31:07 +0200

    feat: add Color Entity

---
+++ src/Entity/Color.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ColorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ColorRepository::class)]
+class Color
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\ManyToMany(targetEntity: Mineral::class, inversedBy: 'colors')]
+    private Collection $minerals;
+
+    public function __construct()
+    {
+        $this->minerals = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Mineral>
+     */
+    public function getMinerals(): Collection
+    {
+        return $this->minerals;
+    }
+
+    public function addMineral(Mineral $mineral): static
+    {
+        if (!$this->minerals->contains($mineral)) {
+            $this->minerals->add($mineral);
+        }
+
+        return $this;
+    }
+
+    public function removeMineral(Mineral $mineral): static
+    {
+        $this->minerals->removeElement($mineral);
+
+        return $this;
+    }
+}
