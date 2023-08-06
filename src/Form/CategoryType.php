00f5686f2799d170897a2d854b7da82f39e60bd6
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-06 23:58:08 +0200

    feat: add new Category Form Type

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Mineral;
+use App\Entity\Category;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('minerals', EntityType::class, [
+                'class' => Mineral::class,
+                'choice_label' => function ($allChoices, $currentChoiceKey)
+                {
+                    return $allChoices->getName();
+                },
+                'expanded'  => true,
+                'multiple'  => true,
+                'by_reference' => false
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success' 
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
