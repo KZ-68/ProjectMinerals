ca65ba0ed221bf003423bf2274a63e08daa693ba
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-07 23:43:45 +0200

    feat: add Model for Advanced Search Data

---
+++ src/Model/AdvancedSearchData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model; 
+
+class AdvancedSearchData {
+
+    public int $page = 1;
+
+    public ?string $name = null;
+
+    public ?string $formula = null;
+
+    public ?string $crystal_system = null;
+
+    public ?string $density = null;
+
+    public ?int $hardness = null;
+
+    public ?string $fracture = null;
+
+    public ?string $streak = null;
+
+}
