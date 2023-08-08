e22bbcc26b03d32263142e350cd7d8afecfdf7c3
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-08 20:42:02 +0200

    feat: add new color type form file

---
+++ src/Form/AddColorType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Color;
+use App\Entity\Mineral;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class AddColorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Color Name',
+                'attr' => [
+                    'class' => 'form-control' 
+                ]
+            ])
+            ->add('minerals', EntityType::class, [
+                'class' => Mineral::class,
+                'choice_label' => function ($allChoices, $currentChoiceKey)
+                {
+                    return $allChoices->getName();
+                },
+                'expanded'  => false,
+                'multiple'  => true,
+                'by_reference' => false,
+                'attr' => [
+                    'class' => 'form-select' 
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success' 
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Color::class,
+        ]);
+    }
+}
