22d6ac7cde74814d20d938bea8a106782300040d
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-23 23:50:03 +0200

    feat: add map_form javascript

---
+++ public/js/map_form.js
@@ -0,0 +1,71 @@
+let mymap, marqueur // Variable de la map et du marqueur
+
+window.onload = () => {
+    mymap = L.map('mineral-map').setView([51.505, -0.09], 11); // Position sur la map par défaut
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 1,
+        maxZoom: 20
+    }).addTo(mymap) // Ajout des tuiles à la map
+    mymap.on("click", mapClickListener) // Ajout de l'événement au clic sur la map
+    document.querySelector('#mineral_country_name').addEventListener('blur', getCountry)
+}
+
+// Fonction de l'événement au clic
+function mapClickListener(e) {
+    // Récupère les coordonnées du clic
+    let pos = e.latlng
+
+    // Ajout d'un marqueur
+        addMarker(pos)
+
+    // Affiche les coordonnées correspondantes dans le formulaire
+    document.querySelector("#mineral_latitude").value = pos.lat // Coordonées de latitude
+    document.querySelector("#mineral_longitude").value = pos.lng // Coordonnées de longitude
+}
+
+// Fonction d'ajout de marqueur
+function addMarker(pos) {
+    // Condition si un marqueur existe
+    if (marqueur != undefined) {
+        mymap.removeLayer(marqueur)
+    }
+    
+    marqueur = L.marker(pos, {
+        // Autorise le déplacement du marqueur
+        draggable: true
+    })
+
+    marqueur.on("draggend", function (e) {
+        pos = e.target.getLatLng()
+        document.querySelector("#mineral_latitude").value = pos.lat
+        document.querySelector("#mineral_longitude").value = pos.lng
+    })
+
+    marqueur.addTo(mymap) // Ajout du marqueur à la map
+}
+
+function getCountry() {
+    let country = document.querySelector('#mineral_country_name').value
+
+    const xmlhttp = new XMLHttpRequest
+
+    xmlhttp.onreadystatechange = () => {
+        // Si la requête est terminée
+        if(xmlhttp.readyState == 4) {
+            if (xmlhttp.status = 200) {
+                let response = JSON.parse(xmlhttp.response)
+
+                let lat = response[0]["latlng"][0]
+                let lng = response[0]["latlng"][1]
+                document.querySelector("#mineral_latitude").value = lat
+                document.querySelector("#mineral_longitude").value = lng
+
+            }
+        }
+    }
+
+    xmlhttp.open("get", `https://restcountries.com/v3.1/name/${country}?format=json`)
+
+    xmlhttp.send()
+}
