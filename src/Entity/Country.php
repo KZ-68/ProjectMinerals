e239fe8ae5fa4d74ec00c497fa56a381f11427ee
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-23 23:55:58 +0200

    feat: add Country Entity

---
+++ src/Entity/Country.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CountryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ApiResource()]
+#[ORM\Entity(repositoryClass: CountryRepository::class)]
+class Country
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $addedAt = null;
+
+    #[ORM\ManyToMany(targetEntity: Mineral::class, mappedBy: 'countries')]
+    private Collection $minerals;
+
+    #[ORM\Column(length: 50)]
+    private ?string $latitude = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $longitude = null;
+
+    public function __construct()
+    {
+        $this->minerals = new ArrayCollection();
+        $this->addedAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddedAt(): ?\DateTimeImmutable
+    {
+        return $this->addedAt;
+    }
+
+    public function setAddedAt(\DateTimeImmutable $addedAt): static
+    {
+        $this->addedAt = $addedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Mineral>
+     */
+    public function getMinerals(): Collection
+    {
+        return $this->minerals;
+    }
+
+    public function addMineral(Mineral $mineral): static
+    {
+        if (!$this->minerals->contains($mineral)) {
+            $this->minerals->add($mineral);
+            $mineral->addCountry($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMineral(Mineral $mineral): static
+    {
+        if ($this->minerals->removeElement($mineral)) {
+            $mineral->removeCountry($this);
+        }
+
+        return $this;
+    }
+
+    public function getLatitude(): ?string
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(string $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?string
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(string $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+}
