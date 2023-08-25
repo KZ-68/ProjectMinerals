9c1b0663cfb58068f8e3895258faee0a090a82d5
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-25 19:58:37 +0200

    fix:change declaration key-word for display popups

--- public/js/map.js
+++ public/js/map.js
@@ -1,36 +1,38 @@
 let coordinates = document.querySelectorAll('.coordinate-data')
-let mymap// Variable de la map
-    window.onload = () => {
-        mymap = L.map('mineral-map').setView([51.505, -0.09], 4); // Position sur la map par défaut
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            minZoom: 1,
-            maxZoom: 20
-        }).addTo(mymap) // Ajout des tuiles à la map
-        for(coordinate in coordinates) { 
-        let lat = coordinates[coordinate].dataset.lat
-        let lng = coordinates[coordinate].dataset.lng
+    let mymap// Variable de la map
+        window.onload = () => {
+            mymap = L.map('mineral-map').setView([51.505, -0.09], 4); // Position sur la map par défaut
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                minZoom: 1,
+                maxZoom: 20
+            }).addTo(mymap) // Ajout des tuiles à la map
+            for(coordinate in coordinates) { 
+                let lat = coordinates[coordinate].dataset.lat
+                let lng = coordinates[coordinate].dataset.lng
 
-        const xmlhttp = new XMLHttpRequest
+                let marker = new L.marker([lat, lng])
+                    .addTo(mymap);
+                    mymap.setView([lat, lng], 4)
+                    
+                const xmlhttp = new XMLHttpRequest
 
-        xmlhttp.onreadystatechange = () => {
-            // Si la requête est terminée
-            if(xmlhttp.readyState == 4) {
-                if (xmlhttp.status = 200) {
-                    let response = JSON.parse(xmlhttp.response)
+                xmlhttp.onreadystatechange = () => {
+                    // Si la requête est terminée
+                    if(xmlhttp.readyState == 4) {
+                        if (xmlhttp.status = 200) {
+                            let response = JSON.parse(xmlhttp.response)
 
-                    let country = response['address']['country']
-                    marker.bindPopup("<p>"+country+"</p>")
+                            let country = response['address']['country']
+                            marker.bindPopup("<p>"+country+"</p>")
+                        }
+                    }
                 }
-            }
-        }
 
-        xmlhttp.open("get", "https://geocode.maps.co/reverse?lat="+lat+"&lon="+lng+"")
-        xmlhttp.send()
+                xmlhttp.open("get", "https://geocode.maps.co/reverse?lat="+lat+"&lon="+lng+"")
+
+                xmlhttp.send()
+            
+            }
         
-            var marker = new L.marker([lat, lng])
-            .addTo(mymap);
-            mymap.setView([lat, lng], 4)
-        }
-    
-    } 
+        } 
