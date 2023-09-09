bd9c8a0d3f41280c7f6624d5b5581f34752c2598
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-09-10 01:03:02 +0200

    add back to top button minimum on scroll display

--- public/js/topFunction.js
+++ public/js/topFunction.js
@@ -1,3 +1,20 @@
+let topBtn = document.querySelector(".topBtn");
+
+if(document.body.scrollTop == 0) {
+    topBtn.style.display = "none";
+} 
+
+window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+          topBtn.style.display = "block";
+        } else {
+          topBtn.style.display = "none";
+        }
+      }
+
+
+
 function topFunction() {
     document.body.scrollTop = 0; // Safari
     document.documentElement.scrollTo({
