3ed88ff00b3b102445ee1cc6621c75bf595b3294
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-09-20 21:49:04 +0200

    feat: add ModificationHistory relation

--- src/Entity/Lustre.php
+++ src/Entity/Lustre.php
@@ -37,9 +37,13 @@ class Lustre
     #[ORM\Column(length: 255, unique: true)]
     private ?string $slug = null;
 
+    #[ORM\OneToMany(mappedBy: 'lustre', targetEntity: ModificationHistory::class)]
+    private Collection $modificationHistories;
+
     public function __construct()
     {
         $this->minerals = new ArrayCollection();
+        $this->modificationHistories = new ArrayCollection();
     }
 
     #[ORM\PrePersist]
@@ -96,6 +100,36 @@ class Lustre
     public function setSlug(string $slug): static
     {
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ModificationHistory>
+     */
+    public function getModificationHistories(): Collection
+    {
+        return $this->modificationHistories;
+    }
+
+    public function addModificationHistory(ModificationHistory $modificationHistory): static
+    {
+        if (!$this->modificationHistories->contains($modificationHistory)) {
+            $this->modificationHistories->add($modificationHistory);
+            $modificationHistory->setLustre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeModificationHistory(ModificationHistory $modificationHistory): static
+    {
+        if ($this->modificationHistories->removeElement($modificationHistory)) {
+            // set the owning side to null (unless already changed)
+            if ($modificationHistory->getLustre() === $this) {
+                $modificationHistory->setLustre(null);
+            }
+        }
 
         return $this;
     }
