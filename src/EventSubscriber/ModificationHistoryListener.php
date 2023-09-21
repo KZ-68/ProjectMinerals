b8434aa9c918ed8d55dd78bceacc5d0d6f629caa
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-09-21 23:18:59 +0200

    add if statements for new or deleted values only + more combinations of each values

--- src/EventSubscriber/ModificationHistoryListener.php
+++ src/EventSubscriber/ModificationHistoryListener.php
@@ -3,6 +3,8 @@
 namespace App\EventSubscriber;
 
 use App\Entity\Color;
+use App\Entity\Coordinate;
+use App\Entity\Image;
 use App\Entity\Lustre;
 use App\Entity\Mineral;
 use Doctrine\ORM\Events;
@@ -31,6 +33,7 @@ class ModificationHistoryListener implements EventSubscriber
         $mineral = $args->getObject();
         $color = $args->getObject();
         $lustre = $args->getObject();
+        $image = $args->getObject();
        
         $user = $this->tokenStorage->getToken()->getUser();
         
@@ -38,10 +41,11 @@ class ModificationHistoryListener implements EventSubscriber
             $entityManager = $args->getObjectManager();
 
             $changeSet = $entityManager->getUnitOfWork()->getEntityChangeSet($mineral);
-            $changeSet = $entityManager->getUnitOfWork()->getEntityChangeSet($color);
 
             $colorsCollection = $mineral->getColors();
             $lustresCollection = $mineral->getLustres();
+            $imagesCollection = $mineral->getImages();
+            $coordinatesCollection = $mineral->getCoordinates();
 
             $insertedColors = $colorsCollection->getInsertDiff();
             $deletedColors = $colorsCollection->getDeleteDiff();
@@ -49,41 +53,203 @@ class ModificationHistoryListener implements EventSubscriber
             $insertedLustres = $lustresCollection->getInsertDiff();
             $deletedLustres = $lustresCollection->getDeleteDiff();
 
+            $insertedImages = $imagesCollection->getInsertDiff();
+            $deletedImages = $imagesCollection->getDeleteDiff();
+
+            $insertedCoordinates = $coordinatesCollection->getInsertDiff();
+            $deletedCoordinates = $coordinatesCollection->getDeleteDiff();
+
             $modificationHistory = new ModificationHistory();
             $modificationHistory->setMineral($mineral);
             $modificationHistory->setUser($user);
 
-            if ($insertedColors && $deletedColors) {
+            
+            if ($deletedColors && !$insertedColors) {
                 $modifiedDataColors = [
                     'color' => [
+                    $deletedColors[0]->getName()
+                    ]
+                ];
+                $modificationHistory->setChanges([$modifiedDataColors, $changeSet]);
+            } else if ($deletedLustres && !$insertedLustres) {
+                $modifiedDataLustres = [
+                    'lustre' => [
+                    $deletedLustres[0]->getType()
+                    ]
+                ];
+                $modificationHistory->setChanges([$modifiedDataLustres, $changeSet]);
+            } else if ($deletedImages && !$insertedImages) {
+                $modifiedDataImages = [
+                    'image' => [
+                    $deletedImages[0]->getFilename()
+                    ]
+                ];
+                $modificationHistory->setChanges([$modifiedDataImages, $changeSet]);
+            } else if ($deletedCoordinates && !$insertedCoordinates) {
+                $modifiedDataCoordinates = [
+                    'coordinate' => [
+                    $deletedCoordinates[0]->getLatitude()
+                    ]
+                ];
+                $modificationHistory->setChanges([$modifiedDataCoordinates, $changeSet]);
+            } else if ($insertedColors && !$deletedColors) {
+                $modifiedDataColors = [
+                    'color' => [
+                    $insertedColors[0]->getName()
+                    ]
+                ];
+                $modificationHistory->setChanges([$modifiedDataColors, $changeSet]);
+            } else if ($insertedLustres && !$deletedLustres) {
+                $modifiedDataLustres = [
+                    'lustre' => [
+                    $insertedLustres[0]->getType()
+                    ]
+                ];
+                $modificationHistory->setChanges([$modifiedDataLustres, $changeSet]);
+            } else if ($insertedImages && !$deletedImages) {
+                $modifiedDataImages = [
+                    'image' => [
+                    $insertedImages[0]->getFilename()
+                    ]
+                ];
+                $modificationHistory->setChanges([$modifiedDataImages, $changeSet]);
+            } else if ($insertedCoordinates && !$deletedCoordinates) {
+                $modifiedDataCoordinates = [
+                    'coordinate' => [
+                    $insertedCoordinates[0]->getLatitude(),
+                    $insertedCoordinates[0]->getLongitude()
+                    ]
+                ];
+                $modificationHistory->setChanges([$modifiedDataCoordinates, $changeSet]);
+            } else if($insertedImages && $insertedCoordinates && !$deletedImages && !$deletedCoordinates) {
+                $modifiedDataImages = [
+                    'image' => [
+                    $insertedImages[0]->getFilename()
+                    ]
+                ];
+                $modifiedDataCoordinates = [
+                    'coordinate' => [
+                    $insertedCoordinates[0]->getLatitude(),
+                    $insertedCoordinates[0]->getLongitude()
+                    ]
+                ];
+                $modificationHistory->setChanges([$modifiedDataImages, $modifiedDataCoordinates, $changeSet]);
+            } else if ($insertedColors && $deletedColors) {
+                $modifiedDataColors = [
+                    'color' => [
+                    $deletedColors[0]->getName(),   
                     $insertedColors[0]->getName(),
-                    $deletedColors[0]->getName(),
                     ]
                 ];
                 $modificationHistory->setChanges([$modifiedDataColors, $changeSet]);
             } else if ($insertedLustres && $deletedLustres) {
                 $modifiedDataLustres = [
                     'lustre' => [
-                    $insertedLustres[0]->getType(),
                     $deletedLustres[0]->getType(),
+                    $insertedLustres[0]->getType(),
                     ]
                 ];
                 $modificationHistory->setChanges([$modifiedDataLustres, $changeSet]);
-            } else if ($insertedColors && $deletedColors && $insertedLustres && $deletedLustres) {
+            } else if (
+                $insertedColors && $deletedColors && 
+                $insertedLustres && $deletedLustres
+                ) {
                 $modifiedDataColors = [
                     'color' => [
-                    $insertedColors[0]->getName(),
                     $deletedColors[0]->getName(),
+                    $insertedColors[0]->getName(),
                     ]
                 ];
                 $modifiedDataLustres = [
                     'lustre' => [
-                    $insertedLustres[0]->getType(),
                     $deletedLustres[0]->getType(),
+                    $insertedLustres[0]->getType(),
                     ]
                 ];
                 $modificationHistory->setChanges([$modifiedDataColors, $modifiedDataLustres, $changeSet]);
-            }else {
+            } else if (
+                $insertedColors && $deletedColors && 
+                $insertedCoordinates) {
+                $modifiedDataColors = [
+                    'color' => [
+                    $deletedColors[0]->getName(),
+                    $insertedColors[0]->getName(),
+                    ]
+                ];
+                $modifiedDataCoordinates = [
+                    'coordinate' => [
+                    $insertedCoordinates[0]->getLatitude(),
+                    $insertedCoordinates[1]->getLongitude(),
+                    ]
+                ];
+                $modificationHistory->setChanges([
+                    $modifiedDataColors, 
+                    $modifiedDataCoordinates,
+                    $changeSet
+                ]);
+            } else if (
+                $insertedColors && $deletedColors && 
+                $insertedLustres && $deletedLustres && 
+                $insertedImages) {
+                $modifiedDataColors = [
+                    'color' => [
+                    $deletedColors[0]->getName(),
+                    $insertedColors[0]->getName(),
+                    ]
+                ];
+                $modifiedDataLustres = [
+                    'lustre' => [
+                    $deletedLustres[0]->getType(),
+                    $insertedLustres[0]->getType(),
+                    ]
+                ];
+                $modifiedDataImages = [
+                    'image' => [
+                    $insertedImages[0]->getFilename(),
+                    ]
+                ];
+                $modificationHistory->setChanges([
+                    $modifiedDataColors, 
+                    $modifiedDataLustres, 
+                    $modifiedDataImages,
+                    $changeSet
+                ]);
+            } else if (
+                $insertedColors && $deletedColors && 
+                $insertedLustres && $deletedLustres && 
+                $insertedImages &&
+                $insertedCoordinates) {
+                $modifiedDataColors = [
+                    'color' => [
+                    $deletedColors[0]->getName(),
+                    $insertedColors[0]->getName(),
+                    ]
+                ];
+                $modifiedDataLustres = [
+                    'lustre' => [
+                    $deletedLustres[0]->getType(),
+                    $insertedLustres[0]->getType(),
+                    ]
+                ];
+                $modifiedDataImages = [
+                    'image' => [
+                    $insertedImages[0]->getFilename(),
+                    ]
+                ];
+                $modifiedDataCoordinates = [
+                    'coordinate' => [
+                    $insertedCoordinates[0]->getLatitude(),
+                    $insertedCoordinates[0]->getLongitude()
+                    ]
+                ];
+                $modificationHistory->setChanges([
+                    $modifiedDataColors, 
+                    $modifiedDataLustres, 
+                    $modifiedDataImages,
+                    $modifiedDataCoordinates,
+                    $changeSet
+                ]);
+            } else {
                 $modificationHistory->setChanges([$changeSet]);
             } 
             $mineral->addModificationHistory($modificationHistory);
@@ -128,6 +294,25 @@ class ModificationHistoryListener implements EventSubscriber
 
                 $entityManager->persist($modificationHistory);
                 $entityManager->persist($lustre);
+                $entityManager->flush();
+            } 
+        }
+
+        if($image instanceof Image) {
+            $entityManager = $args->getObjectManager();
+
+            $changeSet = $entityManager->getUnitOfWork()->getEntityChangeSet($image);
+
+            if ($changeSet != []) {
+                $modificationHistory = new ModificationHistory();
+                $modificationHistory->setImage($image);
+                $modificationHistory->setUser($user);
+                $modificationHistory->setChanges($changeSet);
+                $image->addModificationHistory($modificationHistory);
+                $user->addModificationHistory($modificationHistory);
+
+                $entityManager->persist($modificationHistory);
+                $entityManager->persist($image);
                 $entityManager->flush();
             } 
         }
