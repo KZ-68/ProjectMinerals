ed1c84c8fde183c2d958d3af730c74b334096d6b
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-10-04 23:38:15 +0200

    feat: add css properties for resize search zone

--- public/js/ajaxSearch.js
+++ public/js/ajaxSearch.js
@@ -12,11 +12,13 @@ $(document).ready(function() {
           success: function(response) {
               const resultsContainer = $('#search-results');
               resultsContainer.empty();
-              $.each(response, function(index, result) {
-                  
-                  const resultItem = $('<div>').html(result.name)
-                  resultsContainer.append(resultItem)
+              $.each(response.data, function(index, result) {
+                  const resultItem = $('<div>').html(result.name);
+                  resultsContainer.append(resultItem);
               });
+              $('.adv-search').css('height', '1240px');
+              $('.adv-search-fields').css('height', '1209px');
+              resultsContainer.css('margin', '17px 0px');
           },
           error: function (error) {
             console.error('Erreur lors de la recherche :', error);
