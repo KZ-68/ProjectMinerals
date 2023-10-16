7ae290ef9299f0138f77419863d883467e29f2ea
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-10-16 18:33:42 +0200

    feat: add if statement when there's no parent

--- src/EventListener/NotificationListener.php
+++ src/EventListener/NotificationListener.php
@@ -19,22 +19,25 @@ class NotificationListener
             $entityManager = $args->getObjectManager();
             $commentRepository = $entityManager->getRepository('App\Entity\Comment');
             
-            $parent_comment_user = $comment->getParent()->getUser();
-            $parent_id = $commentRepository->find($comment->getParent()->getId());
-            $children = $parent_id->getChildren();
-            
-            foreach($children as $child) {
-                if ($child === $comment) {
-                    $notification->setUser($parent_comment_user);
-                    $notification->setComment($comment);
+            if ($comment->getParent() !== null) {
+                $parent_comment_user = $comment->getParent()->getUser();
+                $parent_id = $commentRepository->find($comment->getParent()->getId());
+                $children = $parent_id->getChildren();
+                
+                foreach($children as $child) {
+                    if ($child === $comment) {
+                        $notification->setUser($parent_comment_user);
+                        $notification->setComment($comment);
+                    }
                 }
+                
+                $parent_comment_user->addNotification($notification);
+                
+                $entityManager->persist($notification);
+                $entityManager->persist($parent_comment_user);
+                $entityManager->flush();
             }
             
-            $parent_comment_user->addNotification($notification);
-            
-            $entityManager->persist($notification);
-            $entityManager->persist($parent_comment_user);
-            $entityManager->flush();
         }
     }
 }
