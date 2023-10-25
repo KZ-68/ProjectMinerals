5a9a193f3dc1dc24d364db82589bd14e1b40baa0
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-10-25 23:44:55 +0200

    feat: add ajaxSearch file

--- public/js/ajaxSearch.js
+++ public/js/ajaxSearch.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
-  $('#search-form').submit(function(event) {
-      event.preventDefault();
-
-      var formData = $(this).serialize();
-
-      $.ajax({
-          type: 'POST',
-          url: 'home', 
-          data: formData,
-          success: function(response) {
-              const resultsContainer = $('#search-results');
-              resultsContainer.empty();
-              $.each(response.data, function(index, result) {
-                  const resultItem = $('<div>').html(result.name);
-                  resultsContainer.append(resultItem);
-              });
-              $('.adv-search').css('height', '1240px');
-              $('.adv-search-fields').css('height', '1209px');
-              $('#search-results').css('overflow-y', 'auto');
-              $('#search-results').css('background-color', '#9affe2b0');
-              $('#search-results').css('padding', '20px');
-              resultsContainer.css('margin', '17px 0px');
-          },
-          error: function (error) {
-            console.error('Erreur lors de la recherche :', error);
-          }
-      });
+    $('#mineral-search-form').submit(function(event) {
+        event.preventDefault();
+  
+        var formData = $(this).serialize();
+        
+        $.ajax({
+            type: 'POST',
+            url: '/wiki/mineral', 
+            data: formData,
+            success: function(response) {
+                const resultsContainer = $('#minerals-list');
+                resultsContainer.empty();
+                for (var data in response) {
+                    console.log(response);
+                    if (response.hasOwnProperty(data)) { 
+                        const resultItem = $('<li>').html(response[data].name);
+                        resultsContainer.append(resultItem);
+                        resultsContainer.append('</li>');
+                    }
+                }
+                resultsContainer.css('margin', '17px 0px');
+            },
+            error: function (error) {
+              console.error('Erreur lors de la recherche :', error);
+            }
+        });
+    });
   });
-});
