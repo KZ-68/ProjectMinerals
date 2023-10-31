b44abfa752a319edec00d88c3984bdb6363ec6c9
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-10-31 17:57:41 +0100

    feat: add media queries event

--- public/js/ajaxAdvancedSearch.js
+++ public/js/ajaxAdvancedSearch.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   $('#search-form').submit(function(event) {
       event.preventDefault();
+      let mobile = [
+        window.matchMedia('(min-device-width: 320px) and (max-width: 480px)')
+      ]
 
       var formData = $(this).serialize();
 
@@ -15,12 +18,22 @@ $(document).ready(function() {
                   const resultItem = $('<div>').html(result.name);
                   resultsContainer.append(resultItem);
               });
-              $('.adv-search').css('height', '1240px');
-              $('.adv-search-fields').css('height', '1209px');
-              $('#search-results').css('overflow-y', 'auto');
-              $('#search-results').css('background-color', '#9affe2b0');
-              $('#search-results').css('padding', '20px');
-              resultsContainer.css('margin', '17px 0px');
+              if(mobile) {
+                $('.adv-search').css('height', '1600px');
+                $('.adv-search-fields').css('height', '1600px');
+                $('#search-results').css('overflow-y', 'auto');
+                $('#search-results').css('background-color', '#9affe2b0');
+                $('#search-results').css('padding', '20px');
+                resultsContainer.css('margin', '17px 0px');
+              } else {
+                $('.adv-search').css('height', '1240px');
+                $('.adv-search-fields').css('height', '1209px');
+                $('#search-results').css('overflow-y', 'auto');
+                $('#search-results').css('background-color', '#9affe2b0');
+                $('#search-results').css('padding', '20px');
+                resultsContainer.css('margin', '17px 0px');
+              }
+              
           },
           error: function (error) {
             console.error('Erreur lors de la recherche :', error);
