2d4bcd1f52726cc2f761a3a5f38aebfd89ecb61e
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-11-03 00:00:24 +0100

    feat: add edit role user form type

---
+++ src/Form/ChangeUserRoleType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangeUserRoleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $permissions = [
+             'User role' => 'ROLE_USER' ,
+             'Moderator role' => 'ROLE_MODERATOR'
+        ];
+        
+        $builder
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => function ($allChoices, $currentChoiceKey)
+                {
+                    return $allChoices->getUserName();
+                },
+                'expanded'  => false,
+                'multiple'  => false,
+                'label'    => 'Choose the user'
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label'   => 'Choose the role',
+                'choices' => $permissions
+            ])
+            ->add(
+                'save', SubmitType::class
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+       
+    }
+}
