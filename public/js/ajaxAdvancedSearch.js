b21fa5ec41d1a78918b227209054bea3f675f1b9
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-11-05 22:45:47 +0100

    feat: add reset button search

--- public/js/ajaxAdvancedSearch.js
+++ public/js/ajaxAdvancedSearch.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
   $('#search-form').submit(function(event) {
-      event.preventDefault();
-      let mobile = [
-        window.matchMedia('(min-device-width: 320px) and (max-width: 480px)')
-      ]
-
+      event.preventDefault(); 
       var formData = $(this).serialize();
 
       $.ajax({
@@ -18,22 +14,19 @@ $(document).ready(function() {
                   const resultItem = $('<div>').html(result.name);
                   resultsContainer.append(resultItem);
               });
-              if(mobile.matches) {
+              if(window.matchMedia("(max-width : 480px)").matches) {
                 $('.adv-search').css('height', '1600px');
                 $('.adv-search-fields').css('height', '1600px');
-                $('#search-results').css('overflow-y', 'auto');
-                $('#search-results').css('border', '2px solid #9affe2b0');
-                $('#search-results').css('padding', '20px');
-                resultsContainer.css('margin', '17px 0px');
               } else {
                 $('.adv-search').css('height', '1240px');
                 $('.adv-search-fields').css('height', '1209px');
-                $('#search-results').css('overflow-y', 'auto');
-                $('#search-results').css('border', '2px solid #9affe2b0');
-                $('#search-results').css('padding', '20px');
-                resultsContainer.css('margin', '17px 0px');
               }
-              
+              $('#search-results').css('overflow-y', 'auto');
+              $('#search-results').css('border', '2px solid #9affe2b0');
+              $('#search-results').css('padding', '20px');
+              resultsContainer.css('margin', '17px 0px');
+
+              resetBtn(resultsContainer);
           },
           error: function (error) {
             console.error('Erreur lors de la recherche :', error);
@@ -41,3 +34,21 @@ $(document).ready(function() {
       });
   });
 });
+
+function resetBtn(container) {
+  let resetBtnClass = $('<a id="reset-search-btn" href="#">');
+  resetBtnClass.text('Reset');
+  container.append(resetBtnClass);
+  resetBtnClass.on( "click", function(event) {
+    event.preventDefault();
+    if(window.matchMedia("(max-width : 480px)").matches) {
+      $('.adv-search').css('height', '1300px');
+      $('.adv-search-fields').css('height', '1298px');
+    } else {
+      $('.adv-search').css('height', '765px');
+      $('.adv-search-fields').css('height', '620px');
+    }
+    container.empty();
+  } );
+}
+
