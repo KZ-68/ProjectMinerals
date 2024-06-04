6fe520c3902f29ae45f98ca8f82c4fb5399ee87d
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-06-04 22:50:34 +0200

    fix : add conditions for each pathnames

--- public/js/ajaxSearch.js
+++ public/js/ajaxSearch.js
@@ -3,29 +3,54 @@ $(document).ready(function() {
         event.preventDefault();
   
         var formData = $(this).serialize();
+        if(window.location.pathname === "/fr/home") {
+            $.ajax({
+                type: 'POST',
+                url: './wiki/mineral', 
+                data: formData,
+                success: function(response) {
+                    const resultsContainer = $('#minerals-list');
+                    resultsContainer.empty();
+                    $.each(response.data, function(index, result) {
+                        const resultList = $('<li>');
+                        let resultItem = $('<a>');
+                        resultItem.addClass('result-item');
+                        let href = resultItem.attr('href', "/wiki/mineral/"+result.slug+"/show");
+                        href.html(result.name);
+                        resultList.append(href);
+                        resultsContainer.append(resultList);
+                    });
+                    resultsContainer.css('margin', '17px 0px');
+                    $('nav').remove();
+                },
+                error: function (error) {
+                  console.error('Erreur lors de la recherche :', error);
+                }
+            });
+        } else {
+            $.ajax({
+                type: 'POST',
+                url: '../wiki/mineral', 
+                data: formData,
+                success: function(response) {
+                    const resultsContainer = $('#minerals-list');
+                    resultsContainer.empty();
+                    $.each(response.data, function(index, result) {
+                        const resultList = $('<li>');
+                        let resultItem = $('<a>');
+                        resultItem.addClass('result-item');
+                        let href = resultItem.attr('href', "../wiki/mineral/"+result.slug+"/show");
+                        href.html(result.name);
+                        resultList.append(href);
+                        resultsContainer.append(resultList);
+                    });
+                    resultsContainer.css('margin', '17px 0px');
+                },
+                error: function (error) {
+                  console.error('Erreur lors de la recherche :', error);
+                }
+            });
+        }
         
-        $.ajax({
-            type: 'POST',
-            url: '/wiki/mineral', 
-            data: formData,
-            success: function(response) {
-                const resultsContainer = $('#minerals-list');
-                resultsContainer.empty();
-                $.each(response.data, function(index, result) {
-                    const resultList = $('<li>');
-                    let resultItem = $('<a>');
-                    resultItem.addClass('result-item');
-                    let href = resultItem.attr('href', "/wiki/mineral/"+result.slug+"/show");
-                    href.html(result.name);
-                    resultList.append(href);
-                    resultsContainer.append(resultList);
-                });
-                resultsContainer.css('margin', '17px 0px');
-                $('nav').remove();
-            },
-            error: function (error) {
-              console.error('Erreur lors de la recherche :', error);
-            }
-        });
     });
   });
